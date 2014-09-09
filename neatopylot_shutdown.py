6e914ae2c36b29461d891ce2fcb66aa2d15a0779
Simon D. Levy <simon.d.levy@gmail.com>
2014-09-09 16:25:12 -0400

    Update neatopylot_shutdown.py

--- neatopylot_shutdown.py
+++ neatopylot_shutdown.py
@@ -14,6 +14,8 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
+ You should have received a copy of the GNU Lesser General Public License 
+ along with this program.  If not, see <http://www.gnu.org/licenses/>
 '''
 
 DEVICE = '/dev/ttyACM0'               # Raspbian
