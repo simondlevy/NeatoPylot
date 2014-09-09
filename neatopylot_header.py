8ad2237b8123510fbed30249bd83b0a56b761718
Simon D. Levy <simon.d.levy@gmail.com>
2014-09-09 16:22:45 -0400

    Create neatopylot_header.py

---
+++ neatopylot_header.py
@@ -0,0 +1,28 @@
+'''
+neatopylot_header.py - common client/server constants for Neato XV-11 Autopylot 
+
+Copyright (C) 2013 Suraj Bajracharya and Simon D. Levy
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as 
+published by the Free Software Foundation, either version 3 of the 
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+'''
+
+# These are sensible values for an ad-hoc wifi network
+HOST = '192.168.2.2'
+PORT = 20000
+
+# These are arbitrary
+MESSAGE_SIZE_BYTES = 100
+MAX_SCANDATA_BYTES = 10000
+
+
+
+
+
